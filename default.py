993714f3e90f9773b94243ae98aad8152b8a2a83
anxdpanic <anxdpanic@gmail.com>
2019-11-17 11:24:12 -0500

    Update to use SPDX license identifiers

--- default.py
+++ default.py
@@ -1,21 +1,16 @@
-'''
-    Cache service for XBMC
-    Copyright (C) 2010-2011 Tobias Ussing And Henrik Mosgaard Jensen
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+    Cache service for Kodi
     Version 0.8
-'''
+
+    Copyright (C) 2010-2011 Tobias Ussing And Henrik Mosgaard Jensen
+    Copyright (C) 2019 anxdpanic
+
+    This file is part of script.common.plugin.cache
+
+    SPDX-License-Identifier: GPL-3.0-only
+    See LICENSES/GPL-3.0-only.txt for more information.
+"""
+
 import sys
 import xbmc
 import xbmcaddon
